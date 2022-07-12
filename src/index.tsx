602135a26ffde478e3b054b7803fc396f3662c0d
snow <1184861291@qq.com>
2022-07-13 01:33:52 +0800

    add route about register

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,9 @@ import "antd/dist/antd.css";
 import "./i18n/config";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import axios from "axios";
+// import axios from "axios";
 
-axios.defaults.headers['x-icode'] = 'qKhDxI15yz';
+// axios.defaults.headers['x-icode'] = 'qKhDxI15yz';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,7 +20,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const RegisterPage: React.FC = () => {
+    return (
+        <h1>这是登录注册的跳转页面，用来做一个路由的跳转练习～～</h1>
+    )
+}
