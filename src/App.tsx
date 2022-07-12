99f5a3cf87689f04078bf720ce1287bff486e16c
snow <1184861291@qq.com>
2022-07-13 01:32:11 +0800

    del files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
 import React from 'react';
-import './App.css';
-import { HomePage } from './pages';
-// import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<HomePage/>} />
-//           {/* <Route path="/signIn" component={SignInPage} />
-//           <Route path="/register" component={RegisterPage} />
-//           <Route path="/detail/:touristRouteId" component={DetailPage} /> */}
-//           {/* <Route render={()=>(<h1>404 not found 页面去火星了 ! </h1>)} /> */}
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
+import { HomePage, RegisterPage } from './pages';
 
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 function App() {
@@ -27,8 +8,9 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          {/* <Route path="/signIn" component={SignInPage} />
           <Route path="/register" component={RegisterPage} />
+          {/* <Route path="/signIn" component={SignInPage} />
+          
           <Route path="/detail/:touristRouteId" component={DetailPage} /> */}
           <Route render={()=>(<h1>404 not found 页面去火星了 ! </h1>)} />
         </Switch>
